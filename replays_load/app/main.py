10087e3942da58bfa593508cd3a3b78e411ce886
nablio <nablio77@gmail.com>
2025-11-20 05:26:14 -0500

    database dev

--- replays_load/app/main.py
+++ replays_load/app/main.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 from google.cloud import storage
 import psycopg2
 import csv
@@ -16,13 +16,21 @@ def get_csv_files():
     bucket = client.bucket(BUCKET_NAME)
     return [blob for blob in bucket.list_blobs() if blob.name.endswith("ticks.csv")]
 
-def load_csv_to_sql(blob):
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-    )
+def infer_data_types(conn, table: str):
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT column_name, data_type
+        FROM information_schema.columns
+        WHERE table_schema = 'public' AND table_name = '{table_name}';
+    """)
+    return cursor.fetchall()
+
+def load_dataframe(blob: list[str]):
+    # create and merge 
+
+def load_csv_to_sql(blob, conn):
+    
     cursor = conn.cursor()
 
     print(f"processing {blob.name}...")
@@ -44,6 +52,15 @@ def run():
     blobs = get_csv_files()
     for blob in blobs:
         load_csv_to_sql(blob)
+
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+    )
+
+    
 
 if __name__ == "__main__":
     run()
--- replays_load/run.ps1
+++ replays_load/run.ps1
@@ -2,13 +2,15 @@ param(
     [string]$SourceDir
 )
 
+$SECRET_FILE = Join-Path $SourceDir "key.json"
+echo "mount at $SECRET_FILE"
 docker build -t sql-loader -f docker/Dockerfile .
 
 docker run `
     -e GOOGLE_APPLICATION_CREDENTIALS="/secrets/key.json" `
     -e DB_HOST="34.150.184.220" `
-    -e DB_NAME="replays-dbe" `
+    -e DB_NAME="replays-data" `
     -e DB_USER="postgres" `
-    -e DB_PASSWORD="+$3s6L)EFqUf`zKX" `
-    -v "C:\project\secrets\key.json:/secrets/key.json" `
+    -e DB_PASSWORD="cse6242-dbase" `
+    -v "C:/project/secrets/key.json:/secrets/key.json" `
     sql-loader "/data"
